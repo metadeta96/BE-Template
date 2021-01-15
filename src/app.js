3e6c34ed85f0427732ff5ed66c5d404b9fc90407
metadeta96 <fernando.settijunior@gmail.com>
2021-01-15 20:28:28 +0700

    feat(contracts): handle possible profile errors
    Improved the documentation too

--- src/app.js
+++ src/app.js
@@ -8,7 +8,12 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 /**
- * @returns contract by id
+ * Get the contract by id for the calling profile
+ * 
+ * Profile can be either a client or a contractor otherwise it will return 404 automatically
+ * 
+ * @name get/contracts/:id
+ * @returns contract by id for the calling profile
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
     const { Contract } = req.app.get('models');
@@ -16,10 +21,15 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
     const profile = req.profile;
 
     const query = { id };
-    if (profile.type === 'client') {
+    if (profile?.type === 'client') {
+        // Filter by ClientId
         query.ClientId = profile.id;
-    } else if (profile.type === 'contractor') {
+    } else if (profile?.type === 'contractor') {
+        // Filter by ContractorId
         query.ContractorId = profile.id;
+    } else {
+        // It should not happen unless there is an error in the code
+        return res.status(404).end();
     }
 
     const contract = await Contract.findOne({ where: query });
